233d26dfcaaa42cc01474be48a46f2a97a17960c
ChEnYuAnYiKe <kevingoes@126.com>
2024-01-22 15:20:34 +0800

    back to last commit

--- imu_gps_localizer/src/imu_processor.cpp
+++ imu_gps_localizer/src/imu_processor.cpp
@@ -21,34 +21,26 @@ void ImuProcessor::Predict(const ImuDataPtr last_imu, const ImuDataPtr cur_imu, 
 
     // Set last state.
     State last_state = *state;
-    Eigen::Quaterniond curr_ori(last_state.G_R_I);
-    double q0 = curr_ori.w();
-    double q1 = curr_ori.x();
-    double q2 = curr_ori.y();
-    double q3 = curr_ori.z();
-
     // Acc and gyro.
     const Eigen::Vector3d acc_unbias = 0.5 * (last_imu->acc + cur_imu->acc) - last_state.acc_bias;
     const Eigen::Vector3d gyro_unbias = 0.5 * (last_imu->gyro + cur_imu->gyro) - last_state.gyro_bias;
 
-    Eigen::Matrix<double, 16, 1> X = Eigen::Matrix<double, 16, 1>::Zero();
-    X << last_state.G_p_I[0], last_state.G_p_I[1], last_state.G_p_I[2],
-         last_state.G_v_I[0], last_state.G_v_I[1], last_state.G_v_I[2],
-         q0, q1, q2, q3,
-         last_state.acc_bias[0], last_state.acc_bias[1], last_state.acc_bias[2],
-         last_state.gyro_bias[0], last_state.gyro_bias[1], last_state.gyro_bias[2];
-
-    /*state->G_p_I = last_state.G_p_I + last_state.G_v_I * delta_t + 
+    state->G_p_I = last_state.G_p_I + last_state.G_v_I * delta_t + 
                    0.5 * (last_state.G_R_I * acc_unbias + gravity_) * delta_t2;
     state->G_v_I = last_state.G_v_I + (last_state.G_R_I * acc_unbias + gravity_) * delta_t;
 
     const Eigen::Vector3d delta_angle_axis = gyro_unbias * delta_t;
     state->G_R_I = last_state.G_R_I * Eigen::AngleAxisd(delta_angle_axis.norm(), delta_angle_axis.normalized()).toRotationMatrix();
-    */
 
     // Covariance Fx cal.
     Eigen::Matrix<double, 16, 16> Fx_ = Eigen::Matrix<double, 16, 16>::Zero();
     Fx_.block<3, 3>(0, 3) = Eigen::Matrix3d::Identity();
+
+    Eigen::Quaterniond curr_ori(last_state.G_R_I);
+    double q0 = curr_ori.w();
+    double q1 = curr_ori.x();
+    double q2 = curr_ori.y();
+    double q3 = curr_ori.z();
 
     double FVq0 = 2 * Eigen::Vector3d(q0,-q3,q2).transpose()*acc_unbias;
     double FVq1 = 2 * Eigen::Vector3d(q1,q2,q3).transpose()*acc_unbias;
@@ -89,21 +81,6 @@ void ImuProcessor::Predict(const ImuDataPtr last_imu, const ImuDataPtr cur_imu, 
 
     // Update cov
     state->cov = Fx * last_state.cov * Fx.transpose() + Fi * Qi * Fi.transpose();
-
-    // Update state.
-    X = Fx * X;
-    state->G_p_I = X.block<3,1>(0, 0);
-    state->G_v_I = X.block<3,1>(3, 0);
-    state->acc_bias = X.block<3, 1>(10, 0);
-    state->gyro_bias = X.block<3, 1>(13, 0);
-
-    Eigen::Quaterniond q;
-    q.w() = X(6, 0);
-    q.x() = X(7, 0);
-    q.y() = X(8, 0);
-    q.z() = X(9, 0);
-    q.normalize();
-    state->G_R_I = q.toRotationMatrix();
 
     // Time and imu.
     state->timestamp = cur_imu->timestamp;
